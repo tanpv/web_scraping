bd9dd9d72fb967ac2f39efc98f66b9a7bedb69c9
tanpham10 <phamtan500@gmail.com>
2019-07-11 09:14:38 +0700

    put example

---
+++ wsp/example.py
@@ -0,0 +1,23 @@
+# import library
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import  Options
+
+# create option object
+options = Options()
+options.headless = True
+
+# specify execution path
+chrome_driver_path = r'C:\chromedriver_win32\chromedriver.exe'
+
+# create driver
+driver = webdriver.Chrome(chrome_driver_path, chrome_options=options)
+
+# try to get a url
+url = 'https://www.google.com/search?source=hp&ei=95kmXZ-VF4j48gX8zJKwAw&q=selenium&oq=selenium&gs_l=psy-ab.3..0l10.42275.44835..45106...1.0..0.95.652.9......0....1..gws-wiz.....0..0i131.xjVRyx35-9Q'
+driver.get(url)
+
+# get html
+soup = BeautifulSoup(driver.page_source,'lxml')
+print(soup)
+
