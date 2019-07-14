3b5eb692b5c1aeb6bba1f05c38b0bbd8b58f3350
tanpham <phamtan500@gmail.com>
2019-07-14 20:14:13 +0700

    update

--- beautifulsoup_selenium/imdb - project1 part1 get list of best film ever as text.py
+++ beautifulsoup_selenium/imdb - project1 part1 get list of best film ever as text.py
@@ -2,15 +2,17 @@ from bs4 import  BeautifulSoup
 from selenium import webdriver
 
 
-driver = webdriver.PhantomJS(executable_path = r'C:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+options = webdriver.ChromeOptions()
+options.headless  = True
+options.add_argument("--lang=es")
+driver = webdriver.Chrome(executable_path=r'C:\chromedriver_win32\chromedriver.exe', 
+						options=options)
 
 url = 'https://www.imdb.com/chart/top?ref_=nv_mv_250'
-
 driver.get(url)
-
 soup = BeautifulSoup(driver.page_source,'lxml')
+print(soup.prettify())
 
 table = soup.find('table', class_ = 'chart')
-
 for td in table.find_all('td', class_ = 'titleColumn'):
 	print(td.text.strip().replace('\n','').replace('      ',''))
